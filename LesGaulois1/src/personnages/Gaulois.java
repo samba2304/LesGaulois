512b3baaaeadcc3d7680d69a2b6d10f92f8567ae
samba2304 <samba.ndiaye2304@gmail.com>
2024-02-15 11:57:31 +0100

    Creation class Gaulois

---
+++ LesGaulois1/src/personnages/Gaulois.java
@@ -0,0 +1,45 @@
+package personnages;
+
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+
+public class Gaulois {
+	private String nom;
+	private int force;
+	private int effetPotion;
+
+	public Gaulois(String nom, int force) {
+		this.nom = nom;
+		this.force = force;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "\" " + texte + " \" ");
+
+	}
+
+	private String prendreParole() {
+		return "Le gaulois " + nom + " : ";
+
+	}
+
+	public void frapper(Romain romain) {
+		System.out.println(nom + " envoie un grand coup dans la machoire de "
+							+ romain.getNom());
+		romain.recevoirCoup(force/3);
+		}
+	
+	@Override
+	public String toString() {
+		return "Gaulois [nom=" + nom + ", force=" + force + ", effetPotion=" + effetPotion + "]";
+
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
