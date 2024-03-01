757261dd66766aad21390beb6d6a2759b45186d0
samba2304 <samba.ndiaye2304@gmail.com>
2024-03-01 14:17:47 +0100

    <Le village gaulois>

--- LesGaulois1/README.md
+++ LesGaulois1/README.md
@@ -2,3 +2,5 @@
 Dépôt pour le projet Java de TP1 en ILU1
 
 Creation Class Druide,Village,Chef,Histoire,Scenario.
+
+Modification class village
--- LesGaulois1/src/personnages/Village.java
+++ LesGaulois1/src/personnages/Village.java
@@ -1,18 +1,75 @@
 package personnages;
 
+import java.util.Iterator;
+
 public class Village {
 	private String nom;
+	private Chef chef;
+	private Gaulois[] villageois;
+	private int nbVillageois = 0;
+	private int nbVillageoisMax;
 
-	public Village(String nom) {
+	public Village(String nom, int nbVillageoisMax) {
 		this.nom = nom;
+		this.nbVillageoisMax = nbVillageoisMax;
+		villageois = new Gaulois[nbVillageoisMax];
+	}
+
+	public void ajouterHabitant(Gaulois gaulois) {
+		// TODO Auto-generated method stub
+		if (nbVillageois > nbVillageoisMax) {
+			System.out.println("Village au complet");
+		} else {
+			villageois[nbVillageois] = gaulois;
+			System.out.println("Village ajoute");
+			nbVillageois++;
+		}
+	}
+	
+	public Gaulois trouverHabitant(int numeroVillageois) {
+		// TODO Auto-generated method stub
+		return villageois[numeroVillageois];
+	}
+	
+	public String getChef() {
+		return chef.getNom();
+	}
+
+	public void afficherVillageois() {
+		// TODO Auto-generated method stub
+		System.out.println("Dans le villageois du chef "+getChef()+" vivent les legendaires gaulois:  ");
+		for (int i = 0; i < villageois.length; i++) {
+			if (trouverHabitant(i)==null) {
+				break;
+			}
+			System.out.println("- "+trouverHabitant(i).getNom());
+		}
+	}
+
+	public void setChef(Chef chef) {
+		this.chef = chef;
 	}
 
 	public String getNom() {
 		return nom;
 	}
+	
+	public static void main(String[] args) {
+		Village village;
+		village =new Village("Village des Irreductibles",30);
+		Gaulois asterix;
+		asterix = new Gaulois("asterix", 10);
+		Gaulois obelix;
+		obelix = new Gaulois("obelix", 25);
+		Chef abra;
+		abra = new Chef("Abra", 6, village);
+		village.setChef(abra);
+		village.ajouterHabitant(asterix);
+		village.ajouterHabitant(obelix);
 
-	public void setNom(String nom) {
-		this.nom = nom;
+		village.afficherVillageois();
+		//Gaulois gaulois = village.trouverHabitant(1);
+		//System.out.println(gaulois);
+		//Type mismatch: cannot convert from int to Gaulois
 	}
-
 }
